1d66b7d09d442bea23af3d6ec8c01c066e3cc08f
tarkzim <tom8476oo@gmail.com>
2017-09-05 22:12:23 +0800

    t6-common: Add STLport library as prebuilt binary

--- t6-common/t6-common-vendor.mk
+++ t6-common/t6-common-vendor.mk
@@ -222,6 +222,7 @@ PRODUCT_COPY_FILES += \
     vendor/htc/t6-common/proprietary/vendor/lib/librs_adreno.so:system/vendor/lib/librs_adreno.so \
     vendor/htc/t6-common/proprietary/vendor/lib/librs_adreno_sha1.so:system/vendor/lib/librs_adreno_sha1.so \
     vendor/htc/t6-common/proprietary/vendor/lib/libsc-a3xx.so:system/vendor/lib/libsc-a3xx.so \
+    vendor/htc/t6-common/proprietary/vendor/lib/libstlport.so:system/vendor/lib/libstlport.so \
     vendor/htc/t6-common/proprietary/vendor/lib/libuiblur.so:system/vendor/lib/libuiblur.so \
     vendor/htc/t6-common/proprietary/vendor/lib/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so \
     vendor/htc/t6-common/proprietary/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
