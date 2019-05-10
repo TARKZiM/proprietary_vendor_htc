acc72f7d0a73db4c2badac3a5aa0d2ac97a6dba4
TARKZiM <tom8476oo@gmail.com>
2019-05-10 22:48:28 +0800

    Revert "t6-common: kang perf stack from aries"
    This reverts commit cee70429e9df9d4f04eda377c9cfbbc7b01043d3.

--- t6-common/t6-common-vendor.mk
+++ t6-common/t6-common-vendor.mk
@@ -222,13 +222,11 @@ PRODUCT_COPY_FILES += \
     vendor/htc/t6-common/proprietary/vendor/lib/libOmxQcelp13Dec.so:system/vendor/lib/libOmxQcelp13Dec.so \
     vendor/htc/t6-common/proprietary/vendor/lib/libOmxWmaDec.so:system/vendor/lib/libOmxWmaDec.so \
     vendor/htc/t6-common/proprietary/vendor/lib/libqc-opt.so:system/vendor/lib/libqc-opt.so \
-    vendor/htc/t6-common/proprietary/vendor/lib/libqti-perfd-client.so:system/vendor/lib/libqti-perfd-client.so \
     vendor/htc/t6-common/proprietary/vendor/lib/librs_adreno.so:system/vendor/lib/librs_adreno.so \
     vendor/htc/t6-common/proprietary/vendor/lib/librs_adreno_sha1.so:system/vendor/lib/librs_adreno_sha1.so \
     vendor/htc/t6-common/proprietary/vendor/lib/libsc-a3xx.so:system/vendor/lib/libsc-a3xx.so \
     vendor/htc/t6-common/proprietary/vendor/lib/libskia.so:system/vendor/lib/libskia.so \
     vendor/htc/t6-common/proprietary/vendor/lib/libstlport.so:system/vendor/lib/libstlport.so \
-    vendor/htc/t6-common/proprietary/vendor/lib/libthermalclient.so:system/vendor/lib/libthermalclient.so \
     vendor/htc/t6-common/proprietary/vendor/lib/libuiblur.so:system/vendor/lib/libuiblur.so \
     vendor/htc/t6-common/proprietary/vendor/lib/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so \
     vendor/htc/t6-common/proprietary/lib/libfpsvc.so:system/lib/libfpsvc.so \
