8ff414ebada7620b10389f52221c3da2128d7a47
Siluxsept <decoyoctopus@gmx.net>
2019-10-31 00:30:42 +0100

    oce: Add power_off_alarm blobs from LA.UM.8.6.r1-01900-89xx.0

--- oce/Android.mk
+++ oce/Android.mk
@@ -80,13 +80,12 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := PowerOffAlarm
 LOCAL_MODULE_OWNER := htc
-LOCAL_SRC_FILES := proprietary/vendor/app/PowerOffAlarm/PowerOffAlarm.apk
+LOCAL_SRC_FILES := proprietary/app/PowerOffAlarm/PowerOffAlarm.apk
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
 LOCAL_DEX_PREOPT := false
 LOCAL_MODULE_SUFFIX := .apk
-LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
